272aff4e1bea541f77b4c81c8c2a8b59cbc7cc93
AJ Cooper <acooper@lcs.on.ca>
2024-01-29 08:44:42 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by  Adam-James  Cooper on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
