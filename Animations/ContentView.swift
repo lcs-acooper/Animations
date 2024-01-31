0ed59c9c14d5b2c1f7a3771effff4f178156b9c5
AJ Cooper <acooper@lcs.on.ca>
2024-01-30 19:51:01 -0500

    Added Mr. Gordon's helper view.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -5,20 +5,48 @@
 //  Created by  Adam-James  Cooper on 2024-01-29.
 //
 
+import UIKit
 import SwiftUI
+import Lottie
 
-struct ContentView: View {
+struct LottieView: UIViewRepresentable {
 
-    var body: some View {
-        VStack {
-            Image("Ani")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-                    }
-        .padding()
+    @State var animationNamed: String
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        
+        let view = UIView(frame: .zero)
+        
+        // Lottie View
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named(animationNamed)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        
+        // Constraints
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+        
     }
-}
-
-#Preview {
-    ContentView()
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    typealias UIViewType = UIView
+    
+    struct LottieView_Previews: PreviewProvider {
+        static var previews: some View {
+            LottieView(animationNamed: "782-check-mark-success")
+        }
+    }
 }
