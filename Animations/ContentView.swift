beaa6d7f7498c135a9f859eb4d0250a06edb8523
AJ Cooper <acooper@lcs.on.ca>
2024-01-30 19:50:11 -0500

    Added the third-party lottie-ios library

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("Ani")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+                    }
         .padding()
     }
 }
